182f7dd304019a2fbd48fb96dee73f56ab9aa054
kfrysiak <konrad.frysiak@gmail.com>
2025-06-25 18:09:10 +0200

    feat: failure and success result types

--- src/result/result.ts
+++ src/result/result.ts
@@ -1,6 +1,9 @@
 import { Failure } from './failure';
 import { None } from './success';
 
+export type FailureResult<F = Failure<string, undefined>> = readonly [None, F];
+export type SuccessResult<T> = readonly [T, undefined];
+
 export type Result<T, F = Failure<string, undefined>> =
   | readonly [T, undefined]
   | readonly [None, F];
