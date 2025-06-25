754824976f8ab962339f25cab491522415c4cbbe
kfrysiak <konrad.frysiak@gmail.com>
2025-06-25 19:12:26 +0200

    feat: collapseResults tests

--- src/result/failure.test.ts
+++ src/result/failure.test.ts
@@ -1,6 +1,7 @@
 import { describe, expectTypeOf, test } from 'vitest';
 import { success } from './success';
 import { failure, FailureOf } from './failure';
+import { collapseResults } from './utils';
 
 type Exact<T, U, True = true, False = false> = T extends U
   ? U extends T
@@ -11,8 +12,18 @@ type Exact<T, U, True = true, False = false> = T extends U
 type Prettify<T> = { [K in keyof T]: T[K] } & {};
 
 describe('FailureOf', () => {
-  test('returns valid failure from Result', () => {
-    function withPayload() {
+  test('returns valid failure from Result', async () => {
+    async function withoutPayload() {
+      if (true as any) {
+        const result = failure({
+          code: 'something_else_went_wrong',
+          message: 'Something else went wrong',
+        });
+        return result;
+      }
+      return success(true);
+    }
+    async function withPayload() {
       if (true as any) {
         const result = failure({
           code: 'something_wrong',
@@ -21,10 +32,11 @@ describe('FailureOf', () => {
         });
         return result;
       }
-      return success(true);
+      return success('payload');
     }
 
-    const withPayloadResult = withPayload();
+    const withoutPayloadResult = await withoutPayload();
+    const withPayloadResult = await withPayload();
     const [res, failureResult] = withPayloadResult;
 
     if (!failureResult) {
@@ -39,6 +51,15 @@ describe('FailureOf', () => {
         id: number;
       };
     };
+    const promises = [withoutPayload(), withPayload()];
+    const promiseResult = await Promise.allSettled(promises);
+    const [collapseSuccess, collapseError] = collapseResults(promiseResult);
+    if (collapseError) {
+      type ErrorCodes = (typeof collapseError)['payload'][number]['code'];
+      expectTypeOf<ErrorCodes>().toEqualTypeOf<
+        'something_else_went_wrong' | 'unknown_error' | 'something_wrong'
+      >();
+    }
     type ToTest = Prettify<typeof failureResult>;
     expectTypeOf<ToTest>().toEqualTypeOf<Match>();
     type JustFailure = FailureOf<typeof withPayloadResult>;
